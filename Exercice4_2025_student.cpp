f8ee169a2801b317378ff54d2f471eb64246ad6f
Liam1337V <167616041+Liam1337V@users.noreply.github.com>
2025-04-08 15:01:32 +0200

    Update Exercice4_2025_student.cpp

--- Exercice4_2025_student.cpp
+++ Exercice4_2025_student.cpp
@@ -6,6 +6,7 @@
 
 #include "ConfigFile.tpp"
 
+
 using namespace std;
 
 const double PI=3.1415926535897932384626433832795028841971e0;
@@ -50,6 +51,7 @@ double epsilon(const std::vector<double>& r, double r1, double epsilon_a, double
     return eps;
 }
 
+
 //TODO build the rho_epsilon function (rho_lib / epsilon_0)
 double rho_epsilon(double rho_lib, double epsilon_0)
 {
@@ -59,6 +61,10 @@ double rho_epsilon(double rho_lib, double epsilon_0)
 int
 main(int argc, char* argv[])
 {
+	
+	cout << "ici" << endl ; 
+    
+
     // USAGE: Exercise4 [configuration-file] [<settings-to-overwrite> ...]
 
     // Read the default input
@@ -79,10 +85,14 @@ main(int argc, char* argv[])
     // Read geometrical inputs
     const double R  = configFile.get<double>("R");
     const double r1 = configFile.get<double>("r1");
+
     const double rho0 = configFile.get<double>("rho0");
 
     // For the analytical comparison
     const bool uniform_rho_case = configFile.get<bool>("uniform_rho_case");
+    
+    // Permitivité relative du vide 
+    const double epsilon_0 = configFile.get<double>("epsilon_0");
         
     // Dielectric relative permittivity
     const double epsilon_a = configFile.get<double>("epsilon_a");
@@ -109,39 +119,83 @@ main(int argc, char* argv[])
     // Position of elements
     vector<double> r(pointCount);
 
-    // TODO build the nodes vector r
-    for (int i = 0; i <= N1; ++i) r[i] = i * h1;
-    for (int i = 1; i <= N2; ++i) r[N1 + i] = r1 + i * h2;
+    //TODO build the nodes vector r
     
     // Arrays initialization
     vector<double> h(pointCount-1); 	// Distance between grid points
     vector<double> midPoint(pointCount-1);  // Midpoint of each grid element
-   
+        
     // TODO build the h vector and midpoint vector
-    for (int i = 0; i < pointCount - 1; ++i) {
-        h[i] = r[i + 1] - r[i];
-        midPoint[i] = (r[i] + r[i + 1]) / 2.0;
-    }
     
+    cout << "ici " << endl ; 
+    
+	for ( size_t i = 0 ; i < N1 ; ++i  )
+	{ h[i] = h1 ; }
+		
+	for ( size_t i = N1 ; i < pointCount ; ++i  )
+	{ h[i] = h2 ; }
+	
+	for ( size_t i = 0 ; i < pointCount ; ++i  )
+	{ cout << h[i] << endl ; }
+	
+	for ( size_t i = 0 ; i < pointCount ; ++i  )
+	{ 
+		if ( i == 0 )
+		{ midPoint[i] = h1/2 ; }
+		else 
+		{
+			if (i < N1)
+			{ midPoint[i] = midPoint[i-1] + h1 ; }
+			else 
+			{
+				if ( i == N1 )
+				{ midPoint[i] = midPoint[i-1] + h1/2 + h2/2 ; }
+				else 
+				{ midPoint[i] = midPoint[i-1] + h2/2 ; }
+			}
+		}
+	 }
+    
+    
+    // N1 intervalles équidistants entre r = 0 et r = r1
+    // N2 intervalles équidistants entre r = r1 et r = R 
+    
+    cout << "ici" << endl ; 
+
     // Construct the matrix and right-hand side
     vector<double> diagonal(pointCount, 1.0);  // Diagonal
     vector<double> lower(pointCount - 1, 0.0); // Lower diagonal
     vector<double> upper(pointCount - 1, 0.0); // Upper diagonal
     vector<double> rhs(pointCount, 0.0);       // Right-hand-side
     
-    for (int k = 0; k < pointCount-1; ++k) {
-         // TODO build the vectors diagonal, lower, upper, rhs
-        double eps = epsilon({midPoint[k]}, r1, epsilon_a, epsilon_b);
-        diagonal[k] += 1.0 / h[k];
-        lower[k] = -1.0 / h[k];
-        upper[k] = -1.0 / h[k];
-        rhs[k] = rho_epsilon(rho0, eps);
-    }
+    // Loop over the intervals: add the contributions to matrix and rhs   
+	for (int k = 0; k < pointCount-1; ++k) {
+        // TODO build the vectors diagonal, lower, upper, rhs
+        
+        if ( k == 0 )
+        {
+			diagonal[k] = midPoint[k] * epsilon(midPoint[k]) / (2 * h[k]) ; // pas de k-1 => intégrale gauche nulle 
+			lower[k]    = 0 ; // pas de k-1 => intégrale nulle 
+			upper[k]    = - midPoint[k] * epsilon(midPoint[k]) / (2 * h[k]) ; 
+			rhs[k] 		= pow(h[k],2) * rho_epsilon (midPoint[k]) / 2  ; // Faux : remplacer par la bonne formule // pas de k-1 => intégrale gauche nulle 	
+		}   
+		else 
+		{
+			diagonal[k] = midPoint[k-1] * epsilon(midPoint[k-1]) / (2 * h[k-1]) + midPoint[k] * epsilon(midPoint[k]) / (2 * h[k]) ; 
+			lower[k]    = - midPoint[k-1] * epsilon(midPoint[k-1]) / (2 * h[k-1]) ; 
+			upper[k]    = - midPoint[k] * epsilon(midPoint[k]) / (2 * h[k]) ; 
+			rhs[k] 		= pow(h[k-1],2) * rho_epsilon (midPoint[k - 1]) / 2 + pow(h[k],2) * rho_epsilon (midPoint[k]) / 2   ; // Faux : remplacer par la bonne formule 
+		}
 
-     // TODO boundary condition at r=R (modify the lines below)
-    lower[lower.size() - 1] = 0.0;
+    }
+        
+    
+
+    // TODO boundary condition at r=R (modify the lines below)
+    lower[lower.size() - 1]       = 0.0;
     diagonal[diagonal.size() - 1] = 1.0;
     rhs[rhs.size() - 1] = VR;
+
 
     // Solve the system of equations
     vector<double> phi = solve(diagonal, lower, upper, rhs);
@@ -150,9 +204,53 @@ main(int argc, char* argv[])
     vector<double> E(pointCount - 1, 0);
     vector<double> D(pointCount - 1, 0);
     for (int i = 0; i < E.size(); ++i) {
-        E[i] = -(phi[i + 1] - phi[i]) / h[i];
-        D[i] = epsilon({midPoint[i]}, r1, epsilon_a, epsilon_b) * E[i];
+        // TODO calculate E and D
+        E[i] = 0.0;
+        D[i] = 0.0; 
+    }
+
+    // Export data
+    {
+        // Electric potential phi
+        ofstream ofs(fichier_phi);
+        ofs.precision(15);
+
+        if (r.size() != phi.size())
+            throw std::runtime_error("error when writing potential: r and "
+                                     "phi does not have size");
+
+        for (int i = 0; i < phi.size(); ++i) {
+            ofs << r[i] << " " << phi[i] << endl;
+        }
+    }
+
+    {
+        // Electric field E
+        ofstream ofs(fichier_E);
+        ofs.precision(15);
+
+        if (r.size() != (E.size() + 1))
+            throw std::runtime_error("error when writing electric field: size of "
+                                     "E should be 1 less than r");
+
+        for (int i = 0; i < E.size(); ++i) {
+            ofs << midPoint[i] << " " << E[i] << endl;
+        }
+    }
+    {
+        // Displacement field D
+        ofstream ofs(fichier_D);
+        ofs.precision(15);
+
+        if (E.size() != D.size())
+            throw std::runtime_error("error when writing displacement field: size of "
+                                     "D should be equal to E");
+
+        for (int i = 0; i < D.size(); ++i) {
+            ofs << midPoint[i] << " " << D[i] << endl;
+        }
     }
 
     return 0;
 }
+
