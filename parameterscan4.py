76d9c8aa5409aef7b724540a3f649884eb15f33b
Liam1337V <167616041+Liam1337V@users.noreply.github.com>
2025-04-12 21:54:21 +0200

    Update parameterscan4.py

--- parameterscan4.py
+++ parameterscan4.py
@@ -15,31 +15,20 @@ input_filename = 'configuration.in.example'  # Name of the input file
 
 ############################ Valeurs du configfile ############################
 
-#Values = np.genfromtxt("configuration.in.example" , comments = '%')
+Values = np.genfromtxt("configuration.in.example" , comments = '%')
 
-r1 = 0.015
-
+r1 = Values[1,-1] # 0.015
+print(r1)
 ### Signes b pour le cas du a) ( enlevé pour le b ) mais gardent la définition avec le b)
 ##
-##R= Values[1,-1]
-##r1= Values[2,-1]
-##epsilon_a = Values[3,-1]
-##epsilon_b = Values[4,-1]
-##bepsilon_b = Values[5,-1]
-##uniform_rho_case = Values[6,-1]
-##VR = Values[7,-1]
-##rho0= [7,-1]
-##brho0=[8,-1]
-##N1=[9,-1]
-##N2=[10,-1]
-###output=output.out # 11
-##verbose= [12,-1]
+alpha = Values[13,-1] # coeff de proportionnalité
+print(alpha)
 
 ########################### Simulations ##############################
 
 
 N1 = np.array([90,110,120,140,150,170,200])
-N2 = N1
+N2 = N1 * alpha 
 
 nsimul = N1.size
 
@@ -84,7 +73,7 @@ Phi = np.loadtxt(outputs[-1]+"_phi.out")
 def Conv_phi0 ( norder = 3 ) : # étude de convergence ( question a) ordre 2 )
 
     plt.figure()
-    plt.plot(1/pow(N1*2,norder), phi0 , 'k+-')
+    plt.plot(1/pow(N1+N2,norder), phi0 , 'k+-')
     plt.xlabel('(1/N)' + f"$^{norder}$", fontsize = fs)
     plt.ylabel('$\\phi_0$', fontsize = fs)
     #plt.legend()
@@ -92,16 +81,17 @@ def Conv_phi0 ( norder = 3 ) : # étude de convergence ( question a) ordre 2 )
 def Conv_phir1 ( norder = 2 ) : # ( question b) ordre 2 )
 
     plt.figure()
-    plt.plot(1/pow(N1*2,norder), phir1 , 'k+-')
+    plt.plot(1/pow(N1+N2,norder), phir1 , 'k+-')
     plt.xlabel('(1/N)' + f"$^{norder}$", fontsize = fs)
-    plt.ylabel('$\\phi_0$', fontsize = fs)  
+    plt.ylabel('$\\phi(r_1)$', fontsize = fs)  
     
 
 def Eplot () : # Plot le champ électrique en fonction de r 
 
     plt.figure()
+    plt.title(f'N1 = {N1[-1]} et N2 = {N2[-1]}')
     plt.plot(E[:,0],E[:,1], color = "black" )
-    plt.vlines(r1, ymin = min(E[:,1]) , ymax = max(E[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r = {r1} $")
+    plt.vlines(r1, ymin = min(E[:,1]) , ymax = max(E[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r_1 = {r1} $")
     plt.xlabel('r [m]', fontsize = fs)
     plt.ylabel('E [V/m]', fontsize = fs)
     plt.legend()
@@ -110,7 +100,7 @@ def Dplot () : # Plot le champ de déplacement en fonction de r
 
     plt.figure()
     plt.plot(D[:,0],D[:,1], color = "black")
-    plt.vlines(r1, ymin = min(D[:,1]) , ymax = max(D[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r = {r1} $")
+    plt.vlines(r1, ymin = min(D[:,1]) , ymax = max(D[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r_1 = {r1} $")
     plt.xlabel('r [m]', fontsize = fs)
     plt.ylabel('D []', fontsize = fs)
     plt.legend()
@@ -119,7 +109,7 @@ def Phiplot () : # Plot le potentiel en fonction de r
 
     plt.figure()
     plt.plot(Phi[:,0],Phi[:,1], color = "black")
-    plt.vlines(r1, ymin = min(Phi[:,1]) , ymax = max(Phi[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r = {r1} $")
+    plt.vlines(r1, ymin = min(Phi[:,1]) , ymax = max(Phi[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r_1 = {r1} $")
     plt.xlabel('r [m]', fontsize = fs)
     plt.ylabel('$\\phi(r)$ [V]', fontsize = fs)
     plt.legend()
