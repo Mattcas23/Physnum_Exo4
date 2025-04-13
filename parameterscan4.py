ed384a847fd33f231f02f80cf0c75b44ade1546f
Liam1337V <167616041+Liam1337V@users.noreply.github.com>
2025-04-13 17:01:23 +0200

    Update parameterscan4.py

--- parameterscan4.py
+++ parameterscan4.py
@@ -18,24 +18,24 @@ input_filename = 'configuration.in.example'  # Name of the input file
 Values = np.genfromtxt("configuration.in.example" , comments = '//')
 
 R  = Values[0,-1]
-print(R)
+print("R : " , R)
 r1 = Values[1,-1] 
-print(r1)
+print("r1 : " ,r1)
 uniform_rho_case = Values[5,-1]
-print(uniform_rho_case)
+print("unirhocase : " , uniform_rho_case)
 rho0 = Values[7,-1] # valeur pour la question a) 
-print(rho0)
+print("rho0: " , rho0)
 alpha = Values[13,-1] # coeff de proportionnalité
-print(alpha)
+print("alpha : " , alpha)
 
 
 ########################### Simulations ##############################
 
 
-#N1 = np.array([90,110,120,140,150,170,200])
-#N2 = N1 * alpha
+#N1 = np.array([90,110,120,140,150,170,200]) # liste utilisée pour la convergence a)
 
-N1 = np.array([1500])
+
+N1 = np.array([2,5,10,20,50,100])
 N2 = N1*int(alpha)
  
 nsimul = N1.size
@@ -78,7 +78,7 @@ E = np.loadtxt(outputs[-1]+"_E.out")
 D = np.loadtxt(outputs[-1]+"_D.out")
 Phi = np.loadtxt(outputs[-1]+"_phi.out")
 
-def Conv_phi0 ( norder = 3 ) : # étude de convergence ( question a) ordre 2 )
+def Conv_phi0 ( norder = 2 ) : # étude de convergence ( question a) ordre 2 )
 
     plt.figure()
     plt.plot(1/pow(N1+N2,norder), phi0 , 'k+-')
@@ -94,7 +94,7 @@ def Conv_phir1 ( norder = 2 ) : # ( question b) ordre 2 )
     plt.ylabel('$\\phi(r_1)$', fontsize = fs)  
     
 
-def Eplot () : # Plot le champ électrique en fonction de r
+def Eplot (multiple) : # Plot le champ électrique en fonction de r
 
     E0_eq = '$ E(r) = \\rho_0 \\frac{r}{ \\epsilon_0} $'
 
@@ -112,9 +112,20 @@ def Eplot () : # Plot le champ électrique en fonction de r
         
     plt.xlabel('r [m]', fontsize = fs)
     plt.ylabel('E [V/m]', fontsize = fs)
-    plt.legend()
+    plt.legend(fontsize = fs - 2.5)
 
-def Dplot () : # Plot le champ de déplacement en fonction de r
+    if multiple :
+
+        plt.figure()
+        for i in range(len(outputs)) :
+            Ei = np.loadtxt(outputs[i]+"_E.out")
+            plt.plot(Ei[:,0],Ei[:,1], label = f"N1 = {N1[i]} / N2 = {N2[i]}")
+            
+        plt.xlabel('r [m]', fontsize = fs)
+        plt.ylabel('E [V/m]', fontsize = fs)
+        plt.legend()    
+
+def Dplot (multiple) : # Plot le champ de déplacement en fonction de r (multiple = true : met toutes les courbes pour diff N)
 
     D0_eq = '$ D(r) = r \\rho_0  $'
 
@@ -130,10 +141,23 @@ def Dplot () : # Plot le champ de déplacement en fonction de r
         plt.vlines(r1, ymin = min(D[:,1]) , ymax = max(D[:,1]) , color = 'red' , linestyle = 'dashed' , label = f"$ r_1 = {r1} $")
 
     plt.xlabel('r [m]', fontsize = fs)
-    plt.ylabel('D []', fontsize = fs)
-    plt.legend()
+    plt.ylabel('D [C$\,$m$^{-2}$]', fontsize = fs)
+    plt.legend(fontsize = fs - 2.5)
+
+    if multiple :
+
+        plt.figure()
+        for i in range(len(outputs)) :
+            Di = np.loadtxt(outputs[i]+"_D.out")
+            plt.plot(Di[:,0],Di[:,1], label = f"N1 = {N1[i]} / N2 = {N2[i]}")
+            
+        plt.xlabel('r [m]', fontsize = fs)
+        plt.ylabel('D [C$\,$m$^{-2}$]', fontsize = fs)
+        plt.legend()    
+
+        
     
-def Phiplot () : # Plot le potentiel en fonction de r
+def Phiplot (multiple) : # Plot le potentiel en fonction de r
 
     phi0_eq = '$\\phi(r) = -\\frac{\\rho_0}{2 \\epsilon_0} (r^2-R^2)$'
     
@@ -151,7 +175,18 @@ def Phiplot () : # Plot le potentiel en fonction de r
 
     plt.xlabel('r [m]', fontsize = fs)
     plt.ylabel('$\\phi(r)$ [V]', fontsize = fs)
-    plt.legend()
+    plt.legend(fontsize = fs - 2.5)
+
+    if multiple :
+
+        plt.figure()
+        for i in range(len(outputs)) :
+            Phii = np.loadtxt(outputs[i]+"_Phi.out")
+            plt.plot(Phii[:,0],Phii[:,1], label = f"N1 = {N1[i]} / N2 = {N2[i]}")
+            
+        plt.xlabel('r [m]', fontsize = fs)
+        plt.ylabel('$\\phi(r)$ [V]', fontsize = fs)
+        plt.legend() 
 
 def Distance () : # Contrôle pour les midPoints
 
@@ -159,13 +194,13 @@ def Distance () : # Contrôle pour les midPoints
     plt.title(f'N1 = {N1[-1]} et N2 = {N2[-1]}', fontsize = fs - 2)
     plt.scatter(Phi[:,0],np.ones(Phi[:,0].size),label = "$r_i$")
     plt.scatter(D[:,0],np.ones(D[:,0].size),label = "$r_{i+1/2}$" , marker='+')
-    plt.legend()
+    plt.legend(fontsize = fs - 2.5)
 
 #Conv_phi0()
 #Conv_phir1()
-Phiplot()
-Dplot()
-Eplot()
+Phiplot(True)
+Dplot(True)
+Eplot(True)
 #Distance()
 
 
